681d3c5cace16f6927b5155653f154f7e1f1b145
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2015-07-06 00:34:09 +0200

    update arduino door sketch

--- door/arduino-door/arduino-door.ino
+++ door/arduino-door/arduino-door.ino
@@ -4,7 +4,7 @@
  *
  *
  * Sketch for the I2C door device.
- * 
+ *
  * Commands:
  * - 0X01 : Unlocks the door.
  * - 0X02 : Locks the door.
@@ -14,10 +14,19 @@
  * - 0x0A : Retrieves the open / closed state.
  *
  */
+ 
 
 // i2c
 #include <Wire.h>
 #define SLAVE_ADDRESS 0x04
+
+void receive_data(int byte_count);
+void send_data();
+void clear_cmd();
+
+// current cmd that is processed
+int received_cmd = 0x00;
+
 
 // servo
 #include <Servo.h>
@@ -42,51 +51,64 @@ Servo servo;
 #define MAGNETIC_SENSOR_CLOSE_PIN 0
 
 // states
-#define DOOR_LOCK_STATE 1
-#define DOOR_UNLOCK_STATE 0
+#define LOCK_STATE 1
+#define UNLOCK_STATE 0
 
-#define DOOR_CLOSE_STATE 0
-#define DOOR_OPEN_STATE 1
-#define DOOR_TRANSITION_STATE 2
+#define CLOSE_STATE 0
+#define OPEN_STATE 1
+#define TRANSITION_STATE 2
 
 
 // commands
-#define CMD_UNLOCK_DOOR 0x01
-#define CMD_LOCK_DOOR 0x02
+#define CMD_UNLOCK 0x01
+#define CMD_LOCK 0x02
 
-#define CMD_OPEN_DOOR 0x03
-#define CMD_CLOSE_DOOR 0x04
+#define CMD_OPEN 0x03
+#define CMD_CLOSE 0x04
 
 #define CMD_READ_LOCK_STATE 0x09
 #define CMD_READ_CLOSE_STATE 0x0A
 
+// door functions
+int is_in_closed_position();
+int is_in_open_position();
+void unlock_door();
+void lock_door();
+void close_door();
+void open_door();
+void stop_motor();
+
+void send_lock_state_byte();
+void send_close_state_byte();
+
 
 // state variables
-int lock_state = DOOR_UNLOCK_STATE;
+int lock_state = UNLOCK_STATE;
 
-// current cmd that is processed
-int received_cmd = 0x00;
 
+/*
+ * Setups this device.
+ */
 void setup()
 {
   // setup servo
   servo.attach(SERVO_PIN); // servo on digital pin 10
   pinMode(LED_PIN, OUTPUT);
-  
+
   // setup motor
   pinMode(MOTOR_IN1, OUTPUT);
   pinMode(MOTOR_IN2, OUTPUT);
   pinMode(MOTOR_PIN, OUTPUT);
-  
+
   Serial.begin(9600);
-  
-  // initialize i2c as slave
+
+  // initialize i2c device as slave
   Wire.begin(SLAVE_ADDRESS);
-  
+
   // define callbacks for i2c communication
   Wire.onReceive(receive_data);
   Wire.onRequest(send_data);
-  
+
   // init
   unlock_door();
   //delay(2000);
@@ -97,7 +119,7 @@ void setup()
  * Returns 1 if the door is located at the close position.
  */
 int is_in_closed_position() {
-  return analogRead(MAGNETIC_SENSOR_CLOSE_PIN) < 700; 
+  return analogRead(MAGNETIC_SENSOR_CLOSE_PIN) < 700;
 }
 
 
@@ -105,7 +127,7 @@ int is_in_closed_position() {
  * Returns 1 if the door is located at the open position.
  */
 int is_in_open_position() {
-  return analogRead(MAGNETIC_SENSOR_OPEN_PIN) < 700; 
+  return analogRead(MAGNETIC_SENSOR_OPEN_PIN) < 700;
 }
 
 
@@ -115,7 +137,7 @@ int is_in_open_position() {
 void unlock_door() {
   digitalWrite(LED_PIN, HIGH);
   servo.write(UNLOCK_ANGLE);
-  lock_state = DOOR_UNLOCK_STATE;
+  lock_state = UNLOCK_STATE;
 }
 
 
@@ -125,7 +147,7 @@ void unlock_door() {
 void lock_door() {
   servo.write(LOCK_ANGLE);
   digitalWrite(LED_PIN, LOW);
-  lock_state = DOOR_LOCK_STATE;
+  lock_state = LOCK_STATE;
 }
 
 
@@ -161,16 +183,31 @@ void stop_motor() {
  * Sets the current processed cmd to zero.
  */
 void clear_cmd() {
- received_cmd = 0; 
+ received_cmd = 0;
 }
 
 
 /*
- * i2c receive data.
+ * i2c receive callback.
  */
-void receive_data(int byteCount){ 
-   while(Wire.available()) {
+void receive_data(int byte_count) {
+  while(Wire.available()) {
     received_cmd = Wire.read();
+  }
+}
+
+
+/*
+ * i2c send data callback.
+ */
+void send_data(){
+  if(received_cmd == CMD_READ_LOCK_STATE) {
+    send_lock_state_byte();
+    clear_cmd();
+
+  } else if(CMD_READ_CLOSE_STATE) {
+    send_close_state_byte();
+    clear_cmd();
   }
 }
 
@@ -188,32 +225,20 @@ void send_lock_state_byte() {
  * Send close state as a byte over i2c.
  */
 void send_close_state_byte() {
-  char val = DOOR_TRANSITION_STATE;
-  
+  char val = TRANSITION_STATE;
+
   if(is_in_open_position()) {
-    val = DOOR_OPEN_STATE;
+    val = OPEN_STATE;
   }
+
   if(is_in_closed_position()) {
-   val = DOOR_CLOSE_STATE; 
+   val = CLOSE_STATE;
   }
-  
+
   char data[] = { val };
   Wire.write(data, 1);
 }
 
-
-/*
- * i2c send data.
- */
-void send_data(){
-  if(received_cmd == CMD_READ_LOCK_STATE) {
-    send_lock_state_byte();
-    clear_cmd();  
-  } else if(CMD_READ_CLOSE_STATE) {
-    send_close_state_byte();
-    clear_cmd();
-  }
-}
 
 
 
@@ -222,60 +247,58 @@ unsigned long start_time = 0;
 unsigned long closing_time = 0;
 int closing = 0;
 
+
 /*
  * Main loop.
  */
 void loop() {
-   if(millis() - start_time > 1000) {
-     start_time = 0;
-     ignore_door_sensor = 0;
-   }
-    
-  
-  if(received_cmd == CMD_UNLOCK_DOOR){
+  if(millis() - start_time > 1000) {
+    start_time = 0;
+    ignore_door_sensor = 0;
+  }
+
+  if(received_cmd == CMD_UNLOCK) {
     unlock_door();
     clear_cmd();
-    
-  } else if(received_cmd == CMD_LOCK_DOOR) {
+
+  } else if(received_cmd == CMD_LOCK) {
     lock_door();
     clear_cmd();
-    
-  } else if(received_cmd == CMD_OPEN_DOOR) {
+
+  } else if(received_cmd == CMD_OPEN) {
     ignore_door_sensor = 1;
     start_time = millis();
     open_door();
     clear_cmd();
-    
-  } else if(received_cmd == CMD_CLOSE_DOOR) {
+
+  } else if(received_cmd == CMD_CLOSE) {
     ignore_door_sensor = 1;
     start_time = millis();
     close_door();
     clear_cmd();
-    
+
   } else {
-    
+
   }
-  
-  // ensure that the motor is stopped before we oversteer...  
-  if(is_in_open_position() && ignore_door_sensor == 0){
-   delay(10);
-    stop_motor(); 
-  }
-  
+
   // ensure that the motor is stopped before we oversteer...
-  if(is_in_closed_position() && ignore_door_sensor == 0){
+  if(is_in_open_position() && ignore_door_sensor == 0) {
+   delay(10);
+    stop_motor();
+  }
+
+  // ensure that the motor is stopped before we oversteer...
+  if(is_in_closed_position() && ignore_door_sensor == 0) {
    delay(60);
    stop_motor();
    closing_time = millis();
    closing = 1;
-  } 
-  
- 
+  }
+
+
 //  Serial.println("open pin");
 //  Serial.println(analogRead(MAGNETIC_SENSOR_OPEN_PIN));
 //  Serial.println("closed pin");
 //  Serial.println(analogRead(MAGNETIC_SENSOR_CLOSE_PIN));
 //  delay(1000);
 }
-
-
