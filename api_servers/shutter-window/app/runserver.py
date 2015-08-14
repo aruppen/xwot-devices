704de0ef9ad328837d539c8f3a1822ded8bdf177
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2015-08-14 18:05:15 +0200

    change urn for shutetr window

--- api_servers/shutter-window/app/runserver.py
+++ api_servers/shutter-window/app/runserver.py
@@ -13,7 +13,7 @@ yadp.debug()
 from yadp import service
 from yadp.device import Device
 
-device = Device(urn='urn:xwot:Window', location=xwot_app.http_addr, descriptions=[xwot_app.yadp_description])
+device = Device(urn='urn:xwot:ShutterWindow', location=xwot_app.http_addr, descriptions=[xwot_app.yadp_description])
 
 service = service()
 service.register(device=device, passive=True)
--- api_servers/shutter-window/app/runserver.tac
+++ api_servers/shutter-window/app/runserver.tac
@@ -15,7 +15,7 @@ import yadp
 from yadp import service
 from yadp.device import Device
 
-device = Device(urn='urn:xwot:Window', location=xwot_app.http_addr, descriptions=[xwot_app.yadp_description])
+device = Device(urn='urn:xwot:ShutterWindow', location=xwot_app.http_addr, descriptions=[xwot_app.yadp_description])
 
 service = service()
 service.register(device=device, passive=True)
