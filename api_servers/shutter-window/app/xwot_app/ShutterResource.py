5ded9ec0fd03d5b04e1d1788ffd7f29fe3d40f40
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2015-09-28 23:12:09 +0200

    defer shutter.update

--- api_servers/shutter-window/app/xwot_app/ShutterResource.py
+++ api_servers/shutter-window/app/xwot_app/ShutterResource.py
@@ -13,6 +13,7 @@ from xwot.device.shutter import Shutter
 from xwot.util.klein import make_response
 from xwot.util import deserialize
 from xwot.util.klein import cors
+from twisted.internet import task, reactor
 
 shutter = Shutter('Window shutter')
 
@@ -31,10 +32,15 @@ def handle_window_shutter_GET(request):
 #
 @app.route('/window/shutter', methods=['PUT'])
 def handle_window_shutter_PUT(request):
+    cors(request, methods=['GET', 'PUT'])
     data = request.content.read()
     content_type = request.getHeader('Content-Type')
     dic = deserialize(data, content_type=content_type)
-    shutter.update(dic, content_type)
-    cors(request, methods=['GET', 'PUT'])
-    return make_response(shutter, request)
+
+    task.deferLater(reactor, 0.2, shutter.update, dic, content_type)
+    deferred_response = task.deferLater(reactor, 0, make_response, shutter, request)
+    return deferred_response
+
+
+
 
