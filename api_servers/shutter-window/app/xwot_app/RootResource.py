94827f3f40f1d74f20f7d0c5332b7bee93416708
Andreas Ruppen <andreas.ruppen@gmail.com>
2015-10-13 16:32:42 +0200

    serve static files

--- api_servers/shutter-window/app/xwot_app/RootResource.py
+++ api_servers/shutter-window/app/xwot_app/RootResource.py
@@ -11,12 +11,17 @@
 import xwot_app
 from xwot_app import app
 from xwot.util.klein import cors
+from twisted.web.static import File
 
 @app.route('/')
 def home(request):
     request.setHeader('Content-Type', 'application/ld+json')
     cors(request, methods=['GET', 'OPTIONS'])
     return xwot_app.jsonld_description_str
+
+@app.route('/static/', branch=True)
+def static_files(request):
+    return File("./static")
 
 #
 # OPTIONS '/'
